0d4629a8f6271408f3e8873fa3926be6c72a8488
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-05-27 21:32:54 +0900

    feat: initial contract

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  // etherscan: {
+  //   apiKey: `${process.env.ETHERSCAN_API}`,
+  // },
+  networks: {
+    // mainnet: {
+    //   url: `${process.env.API_URL}`,
+    //   accounts: [`${process.env.PRIVATE_KEY}`],
+    // },
+    // local: {
+    //   url: 'http://localhost:8545',
+    //   // accounts: [`${process.env.LOCAL_PRIVATE_KEY}`],
+    // },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    // currency: "JPY",
+    // gasPriceApi:
+    //   "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
 };
 
 export default config;
