8e223453acddb77eea3372f6072ac222f8a7597c
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-08-07 22:06:36 +0900

    feat: add interfaces of descriptor

---
+++ packages/contracts/contracts/interfaces/IDescriptorMinimal.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// LICENSE
+// IDescriptorMinimal.sol is a modified version of NounsDAO's INounsDescriptorMinimal.sol
+// INounsDescriptorMinimal.sol source code Copyright NounsDAO licensed under the GPL-3.0 license.
+// With modifications by Medical DAO.
+
+/// @title Common interface for Descriptor versions.
+
+pragma solidity ^0.8.6;
+
+import {INounsSeeder} from "./INounsSeeder.sol";
+
+interface IDescriptorMinimal {
+    ///
+    /// USED BY TOKEN
+    ///
+
+    function tokenURI(
+        uint256 tokenId,
+        INounsSeeder.Seed memory seed
+    ) external view returns (string memory);
+
+    function dataURI(
+        uint256 tokenId,
+        INounsSeeder.Seed memory seed
+    ) external view returns (string memory);
+
+    ///
+    /// USED BY SEEDER
+    ///
+
+    function backgroundCount() external view returns (uint256);
+
+    function bodyCount() external view returns (uint256);
+
+    function accessoryCount() external view returns (uint256);
+
+    function headCount() external view returns (uint256);
+
+    function glassesCount() external view returns (uint256);
+}
