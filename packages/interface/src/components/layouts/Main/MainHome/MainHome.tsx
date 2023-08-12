9a70a0083cfbf2069bd210c92d3be8ab2e5d360b
yosuke <yosuke2002jp@yahoo.co.jp>
2023-08-12 20:17:14 +0900

    オレオレモックアップの途中まで

--- packages/interface/src/components/layouts/Main/MainHome/MainHome.tsx
+++ packages/interface/src/components/layouts/Main/MainHome/MainHome.tsx
@@ -1,3 +1,4 @@
+import { Londrina_Solid } from "next/font/google";
 import Image from "next/image";
 import { AuctionImage } from "@/features/auction/components/AuctionImage";
 import { BidButton } from "@/features/auction/components/BidButton";
@@ -11,6 +12,16 @@ export type MainHomeProps = {} & BaseProps;
  * MainHome
  * @keit0728
  */
+
+const LondrinaSolid_normal = Londrina_Solid({
+  weight: "400",
+  subsets: ["latin"],
+});
+const LondrinaSolid_bold = Londrina_Solid({
+  weight: "900",
+  subsets: ["latin"],
+});
+
 export const MainHome = ({ className }: MainHomeProps) => {
   return (
     <div className={clsx(className)}>
@@ -21,33 +32,50 @@ export const MainHome = ({ className }: MainHomeProps) => {
           </div>
           <div className="pl-[12px] pr-[80px] flex-col justify-center">
             <div className="flex-col">
-              <div className="flex justify-center items-center">
-                <div className="flex">
-                  <Image
-                    src="/images/arrow_circle_left_72px.png"
-                    alt="test"
-                    width={36}
-                    height={36}
-                  />
-                  <div className="mr-[5px]">
+              <div className="flex">
+                <div className="flex justify-center items-center">
+                  <div className="flex">
                     <Image
-                      src="/images/arrow_circle_right_72px.png"
+                      src="/images/arrow_circle_left_72px.png"
                       alt="test"
                       width={36}
                       height={36}
                     />
+                    <div className="mr-[5px]">
+                      <Image
+                        src="/images/arrow_circle_right_72px.png"
+                        alt="test"
+                        width={36}
+                        height={36}
+                      />
+                    </div>
                   </div>
+                  <h4 className="text-[#79809c]">2023年8月11日</h4>
                 </div>
-                <h4 className="text-[#79809c]">2023年8月11日</h4>
               </div>
               <div className="flex">
-                <h1 className="font-['Londrina Solid'] mt-[4px] mb-[8px] text-[68px] text-[#151c3b]">
+                <h1 className="font-[sans] mt-[4px] mb-[8px] text-[68px] text-[#151c3b]">
                   Noun 808
                 </h1>
               </div>
             </div>
-            <div className="">
-              <BidButton />
+            <div className="flex -mx-[12px]">
+              <div className="flex-col mt-[6px] ml-[6px] px-[12px]">
+                <div className="mb-[8px] text-[18px] font-bold text-[#79809c]">
+                  <h4>現在の入札額</h4>
+                </div>
+                <div className="font-['PT_Root_UI'] font-bold text-[32px] !mb-0 mt-[3px]">
+                  Ξ 32.60
+                </div>
+              </div>
+              <div className="flex-col mt-[5px] pl-[40px] pr-[12px]">
+                <div className="mt-[1px] mb-[8px] text-[18px] font-bold text-[#79809c]">
+                  <h4>オークション終了まで</h4>
+                </div>
+                <div className="font-['PT_Root_UI'] font-bold text-[32px] !mb-0 mt-[3px]">
+                  5時間20分10秒
+                </div>
+              </div>
             </div>
             <div className="">
               <BidText />
--- packages/interface/tailwind.config.js
+++ packages/interface/tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      sans: ["Londrina Solid"],
+    },
   },
   plugins: [],
 };
