4efbe980ff3d108e441519f6727439172639c1bb
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-08-13 12:15:20 +0900

    feat: some update about Descriptor deploy script

--- packages/contracts/scripts/deployAll.ts
+++ packages/contracts/scripts/deployAll.ts
@@ -1,12 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
   const Descriptor = await ethers.getContractFactory("Descriptor");
   const descriptor = await Descriptor.deploy();
 
   await descriptor.deployed();
 
-  console.log(`Descriptor deployed to ${descriptor.address}`);
+  console.log(
+    `Descriptor deployed to ${descriptor.address} by ${deployer.address}`
+  );
 }
 
 main().catch((error) => {
