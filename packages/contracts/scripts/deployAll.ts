8e372532d0e8684e91b3db4b8c495d4b898ea444
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-08-13 12:36:00 +0900

    wip: deploy script about Token

--- packages/contracts/scripts/deployAll.ts
+++ packages/contracts/scripts/deployAll.ts
@@ -1,15 +1,26 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import "dotenv/config";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with account: ", deployer.address);
+
   const Descriptor = await ethers.getContractFactory("Descriptor");
   const descriptor = await Descriptor.deploy();
 
   await descriptor.deployed();
 
-  console.log(
-    `Descriptor deployed to ${descriptor.address} by ${deployer.address}`
+  console.log(`Descriptor address: ${descriptor.address}`);
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy(
+    descriptor.address,
+    descriptor.address,
+    descriptor.address,
+    descriptor.address,
+    descriptor.address
   );
+  console.log(`Token address: ${token.address}`);
 }
 
 main().catch((error) => {
