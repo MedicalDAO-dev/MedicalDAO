e64c171c2165753710f38241b11794cf8a383b3d
yosuke <yosuke2002jp@yahoo.co.jp>
2023-09-02 22:31:22 +0900

    _app.tsxにwagmiを入れて、不要なstyleを削除しました！

--- packages/interface/src/pages/_app.tsx
+++ packages/interface/src/pages/_app.tsx
@@ -1,24 +1,24 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { RecoilRoot } from "recoil";
-
-import { WagmiConfig, createConfig, configureChains } from 'wagmi'
-import { mainnet, polygon, optimism } from 'wagmi/chains'
-
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
-
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { mainnet, optimism, polygon } from "wagmi/chains";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 // Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, polygon, optimism],
-  [alchemyProvider({ apiKey: 'Y3Pj2J-xkxiRQMEe88uKo7TjkTVbIK5C' }), publicProvider()],
-)
+  [
+    alchemyProvider({ apiKey: "Y3Pj2J-xkxiRQMEe88uKo7TjkTVbIK5C" }),
+    publicProvider(),
+  ],
+);
 
 // Set up wagmi config
 const config = createConfig({
@@ -28,26 +28,26 @@ const config = createConfig({
     new CoinbaseWalletConnector({
       chains,
       options: {
-        appName: 'wagmi',
+        appName: "wagmi",
       },
     }),
     new WalletConnectConnector({
       chains,
       options: {
-        projectId: '...',
+        projectId: "...",
       },
     }),
     new InjectedConnector({
       chains,
       options: {
-        name: 'Injected',
+        name: "Injected",
         shimDisconnect: true,
       },
     }),
   ],
   publicClient,
   webSocketPublicClient,
-})
+});
 
 // Pass config to React Context Provider
 // function App() {
@@ -62,11 +62,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <WagmiConfig config={config}>
-        <style jsx global>{`
-          html {
-            font-family: "Arial";
-          }
-        `}</style>
         <Component {...pageProps} />
       </WagmiConfig>
     </RecoilRoot>
