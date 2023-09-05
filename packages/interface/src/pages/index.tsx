e54ec92eb632c4e21ee4c929e75c07a1660fa071
yosuke <yosuke2002jp@yahoo.co.jp>
2023-09-05 20:44:03 +0900

    ブラウザでスマホサイズの画面を指定したところ、大枠がズレてしまっていて修正を試みたものの失敗

--- packages/interface/src/pages/index.tsx
+++ packages/interface/src/pages/index.tsx
@@ -2,15 +2,16 @@ import { Footer } from "@/components/layouts/Footer";
 import { Header } from "@/components/layouts/Header";
 import { Main } from "@/components/layouts/Main";
 import { MainHome } from "@/components/layouts/Main/MainHome";
+import clsx from "clsx";
 
 export default function Home() {
   return (
-    <>
+    <div className={clsx("flex-col", "justify-center", "items-center")}>
       <Header />
       <Main>
         <MainHome />
       </Main>
       <Footer />
-    </>
+    </div>
   );
 }
