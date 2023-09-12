4a45042ed97d217a6f5354cfbfe8dfc6a31d6aa3
keit0728 <keit0728.job@gmail.com>
2023-09-12 17:54:11 +0900

    エラー修正 #54

--- packages/interface/src/models/AuctionModel.ts
+++ packages/interface/src/models/AuctionModel.ts
@@ -1,10 +1,12 @@
+import { NULL_ADDRESS } from "@/const/chain";
 import { ObjectCopier } from "@/models/ObjectCopier";
+import { Address } from "wagmi";
 
 export class AuctionModel extends ObjectCopier {
   constructor(
     public readonly bidAmount: number = 0,
     public readonly imageURL: string = "/images/icon.jpg",
-    public readonly bidder: string = "",
+    public readonly bidder: Address = NULL_ADDRESS,
   ) {
     super();
   }
