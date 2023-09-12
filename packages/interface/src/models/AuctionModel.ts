73d4e0ed90408051fc5ccfadf0122ccaf62656ee
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-09-12 20:30:21 +0900

    fix: typo in AuctionModel.ts

--- packages/interface/src/models/AuctionModel.ts
+++ packages/interface/src/models/AuctionModel.ts
@@ -1,6 +1,5 @@
-import { NFTModel } from "@/models/NFTModel";
 import { NULL_ADDRESS } from "@/const/chain";
-
+import { NFTModel } from "@/models/NFTModel";
 import { ObjectCopier } from "@/models/ObjectCopier";
 import { Address } from "wagmi";
 
@@ -8,7 +7,7 @@ export class AuctionModel extends ObjectCopier {
   constructor(
     public readonly bidAmount: number = 0,
     public readonly imageURL: string = "/images/icon.jpg",
-  public readonly bidder: Address = NULL_ADDRESS,
+    public readonly bidder: Address = NULL_ADDRESS,
     public readonly nft: NFTModel = new NFTModel(),
   ) {
     super();
