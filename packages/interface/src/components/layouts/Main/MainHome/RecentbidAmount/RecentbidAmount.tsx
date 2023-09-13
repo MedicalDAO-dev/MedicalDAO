29a11ea54e2276e224b9c96ffc98e53c73c51417
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-09-13 15:04:55 +0900

    fix: reset some design

--- packages/interface/src/components/layouts/Main/MainHome/RecentbidAmount/RecentbidAmount.tsx
+++ packages/interface/src/components/layouts/Main/MainHome/RecentbidAmount/RecentbidAmount.tsx
@@ -9,28 +9,13 @@ export type RecentBidAmountProps = {} & BaseProps;
  */
 export const RecentBidAmount = ({ className }: RecentBidAmountProps) => {
   return (
-    <div
-      className={clsx(
-        className,
-        "flex-col",
-        "mt-[6px]",
-        "pr-[24px]",
-        "[border-right:1px_solid_rgba(121,128,156,.28627450980392155)]",
-      )}
-    >
-      <h4
-        className={clsx(
-          "mb-[8px]",
-          "text-[18px]",
-          "font-bold",
-          "text-[#79809c]",
-        )}
-      >
+    <div className={clsx(className)}>
+      <div className={clsx("mb-2", "text-lg font-bold text-[#79809c]")}>
         現在の入札額
-      </h4>
+      </div>
       <div className={clsx("font-['PT_Root_UI'] font-bold text-3xl")}>
         Ξ 32.20
       </div>
-    </div >
+    </div>
   );
 };
