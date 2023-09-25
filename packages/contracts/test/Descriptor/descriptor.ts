44b890f7a6513771d257ba3fce71b8184cb74937
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-09-25 23:56:29 +0900

    feat: updated Descriptor test

--- packages/contracts/test/Descriptor/descriptor.ts
+++ packages/contracts/test/Descriptor/descriptor.ts
@@ -8,17 +8,67 @@ describe("Descriptor", () => {
   let descriptor: Descriptor;
 
   let deployer: SignerWithAddress;
+  let user: SignerWithAddress;
+
+  const cid = "QmWdcHRNaEEFbpexkJCJSL26KL6abKYzAVWQiwWE73DM2n";
+
+  const constructTokenURI = (tokenId: number) => {
+    return (
+      '{"name":"Medical DAO NFT ' +
+      tokenId +
+      '", "description":"Medical DAO NFT ' +
+      tokenId +
+      ' is a member of the Medical DAO", "image": "' +
+      getImage(tokenId) +
+      '"}'
+    );
+  };
+
+  const getImage = (tokenId: number) => {
+    return "ipfs://" + cid + "/" + tokenId + ".png";
+  };
+
+  const encodeBase64 = (tokenURI: string): string => {
+    const jsonString = JSON.stringify(tokenURI);
+    return "data:application/json;base64," + btoa(jsonString);
+  };
 
   beforeEach(async () => {
-    [deployer] = await ethers.getSigners();
+    [deployer, user] = await ethers.getSigners();
     ({ descriptor } = await deploy(deployer));
   });
 
   describe("init variables", () => {
     it("should return the correct cid", async () => {
-      expect(await descriptor.cid()).to.equal(
-        "QmWdcHRNaEEFbpexkJCJSL26KL6abKYzAVWQiwWE73DM2n",
-      );
+      expect(await descriptor.getCID()).to.equal(cid);
+    });
+  });
+
+  describe("setCid", () => {
+    it("should revert to set cid if not owner", async () => {
+      await expect(
+        descriptor.connect(user).setCID("newCid"),
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("should set cid", async () => {
+      await descriptor.setCID("newCid");
+      expect(await descriptor.cid())
+        .to.emit(descriptor, "CIDSet")
+        .withArgs("newCid");
+    });
+  });
+
+  // describe("tokenURI", () => {
+  //   it("should return the correct token uri", async () => {
+  //     const encodedTokenURI = encodeBase64(constructTokenURI(1));
+  //     expect(await descriptor.tokenURI(1)).to.equal(encodedTokenURI);
+  //   });
+  // });
+
+  describe("getImage", () => {
+    it("should return the correct image", async () => {
+      expect(await descriptor.getImage(1)).to.equal(getImage(1));
     });
   });
 });
